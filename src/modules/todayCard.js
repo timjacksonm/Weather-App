9b0c2ab2eaf05d86b06ba846117d61b5f5991c91
Tim Jackson <timjacksonm@gmail.com>
2021-03-26 07:15:21 -0500

    added code to todayCard.js that changes background, textprimary and text secondary colors  based on current weather

--- src/modules/todayCard.js
+++ src/modules/todayCard.js
@@ -27,6 +27,7 @@ const todayCard = function displayCard(WeatherDataObject) {
     7: mistOrFog,
     8: sunny,
   };
+  // Changes weather icon based on weather.
   const key = weatherData.weather[0].id.toString().charAt(0);
   if (
     weatherData.weather[0].id <= 802 &&
@@ -43,6 +44,60 @@ const todayCard = function displayCard(WeatherDataObject) {
     document.getElementById('todayIcon').src = clouds;
   }
 
+  // Changes color styling based on weather.
+  const root = document.documentElement;
+  switch (key) {
+    case '2':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #bdc7d3 45%, #d2d6db 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#5a6d7b');
+      root.style.setProperty('--textSecondary', '#8698a6');
+      break;
+    case '3':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #bdc7d3 45%, #d2d6db 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#5a6d7b');
+      root.style.setProperty('--textSecondary', '#8698a6');
+      break;
+    case '5':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #bdc7d3 45%, #d2d6db 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#5a6d7b');
+      root.style.setProperty('--textSecondary', '#8698a6');
+      break;
+    case '6':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #dfeffc 45%, rgb(240, 241, 241) 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#638797');
+      root.style.setProperty('--textSecondary', '#94a7b5');
+      break;
+    case '7':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #dfeffc 45%, rgb(240, 241, 241) 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#638797');
+      root.style.setProperty('--textSecondary', '#94a7b5');
+      break;
+    case '8':
+      root.style.setProperty(
+        '--Background',
+        'linear-gradient(to right, #ffe28f 45%, #fffcad 100%)'
+      );
+      root.style.setProperty('--textPrimary', '#b67c10');
+      root.style.setProperty('--textSecondary', '#b67c10b4');
+      break;
+    default:
+      break;
+  }
   document.getElementById(
     'todayWeatherStatus'
   ).textContent = descriptionCapitalized;
